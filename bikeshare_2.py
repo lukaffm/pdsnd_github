c4e04b10e98b0ff364b744c6cebc2c2253579610
Luka Vujeva <luka@Lukas-MacBook-Pro-250.local>
2019-06-29 09:56:18 +0200

    Ading capitalized selector Y and N for better readability.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -175,3 +175,22 @@ def user_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+
+        restart = input('\nWould you like to restart? Enter YES or NO.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
